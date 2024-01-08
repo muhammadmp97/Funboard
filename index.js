8ff8ff5c25a2c41db0f29e9e0bf7a8face46b74a
Muhammad M <muhammadmp1997@gmail.com>
2024-01-08 12:40:58 +0330

    add home page route

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import { fileURLToPath } from 'url'
+import path from 'path'
 import mongodb from './src/clients/mongodb.js'
 import * as gamesService from './src/services/games.js'
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+app.use(express.static(__dirname + '/public'))
 const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/src/templates/home.html'))
+})
 
 app.get('/api/games', async (req, res) => {
   const liveGames = await gamesService.getAll()
